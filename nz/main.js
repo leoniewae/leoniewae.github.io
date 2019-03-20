2f018ceff9e5480da7c4b2f35ec9d83a70deee11
Wächter <csav8898@uibk.ac.at>
2019-03-20 18:04:52 +0100

    Leaflet einbinden in Daten

---
+++ nz/main.js
@@ -0,0 +1,8 @@
+//alert ("Hallo Welt!")
+
+const div= document.getElementById("map");
+const lat = div.getAttribute ("data-lat");
+const lng = div.getAttribute ("data-lng");
+const title = div.getAttribute ("data-title");
+
+//console.log(lat,lng,title);
