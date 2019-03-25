0962055efdd739a8219646955fc95942175e96d4
leoniewae <leonie_waechter@live.de>
2019-03-25 20:44:54 +0100

    Create a Variable: var 2/10

--- workload/3/workload3.js
+++ workload/3/workload3.js
@@ -1,3 +1,4 @@
-console.log(Math.floor(Math.random() * 100));
-console.log(Math.ceil(43.8));
-console.log(Number.isInteger(2017));
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
