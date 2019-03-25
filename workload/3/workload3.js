bcbfbc4ab7c518659afe75c1a6c20cb440329050
leoniewae <leonie_waechter@live.de>
2019-03-25 21:18:04 +0100

    typeof operator 9/10

--- workload/3/workload3.js
+++ workload/3/workload3.js
@@ -1,3 +1,4 @@
-const myName = 'Leonie'
-const myCity = 'Innsbruck'
-console.log(`My name is  ${myName}. My favorite city is ${myCity}.`);
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
