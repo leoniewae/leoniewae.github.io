e6f7f0ab53d98be47180da38537247127fa760b4
Wächter <csav8898@uibk.ac.at>
2019-03-27 18:55:18 +0100

    Adlerweg fertig

--- adlerweg/adlerweg.js
+++ adlerweg/adlerweg.js
@@ -1,21 +1,21 @@
 //alert ("Hallo Welt!")
 
-const div= document.getElementById("map");
-const breite1 = div.getAttribute ("data-lat1");
-const laenge1 = div.getAttribute ("data-lng1");
-const titel1 = div.getAttribute ("data-title1");
+const div = document.getElementById("map");
+const breite1 = div.getAttribute("data-lat1");
+const laenge1 = div.getAttribute("data-lng1");
+const titel1 = div.getAttribute("data-title1");
 
-const breite2 = div.getAttribute ("data-lat2");
-const laenge2 = div.getAttribute ("data-lng2");
-const titel2 = div.getAttribute ("data-title2");
+const breite2 = div.getAttribute("data-lat2");
+const laenge2 = div.getAttribute("data-lng2");
+const titel2 = div.getAttribute("data-title2");
 //console.log(breite,laenge,titel);
 //Karte initialisieren
-let karte = L.map ("map");
+let karte = L.map("map");
 //console.log (karte);
 
 //auf Ausschnitt zoomen
-karte.setView (
-    [breite1,laenge1],
+karte.setView(
+    [breite1, laenge1],
     13 // Zoomfaktor 1= ganze Welt bis 25= sehr nah
 
 );
@@ -25,63 +25,30 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 //positionsmarker hinzufügen
 let pin1 = L.marker(
-   [breite1,laenge1]
+    [breite1, laenge1]
 ).addTo(karte);
 
 let pin2 = L.marker(
-    [breite2,laenge2]
- ).addTo(karte);
+    [breite2, laenge2]
+).addTo(karte);
 
 //popup zum Pin hängen
 pin1.bindPopup(titel1).openPopup();
 pin2.bindPopup(titel2).openPopup();
 
-const blick1 = {
-    kunde: "Wilder Kaiser",
-    standort: "Gruttenhütte",
-    seehoehe: "1640",
-    lat: 47.55564,
-    lng: 12.31861
-};
+let blickeGruppe = L.featureGroup().addTo(karte)
 
-let pin3 = L.marker (
-    [blick1.lat, blick1.lng]
-).addTo(karte);
-
-
-    const blick2 = {
-        kunde: "Bergbahn Scheffau",
-        standort: "Brandstadl",
-        seehoehe: "1640",
-        lat: 44.4912,
-        lng: 12.24522 };
-
-    let pin4 = L.marker (
-        [blick2.lat, blick2.lng]
-    ).addTo(karte);
+for (let blick of ADLERBLICKE) {
     
-
-        const blick3 = {
-            kunde: "Lechtal Tourismus",
-            standort: "Sonnalm Jöchelspitz",
-            seehoehe: "1786",
-            lat: 47.41028,
-            lng: 10.60083};
-
-
-        const adlerblicke =[
-            blick1,
-            blick2,
-            blick3
-        ];
-
-        for (let blick of adlerblicke) {
-let blickpin = L.marker ( [blick.lat, blick.lng]
-    ).addTo(karte);
+    let blickpin = L.marker([blick.lat, blick.lng]).addTo(blickeGruppe);
     blickpin.bindPopup(
-        `<h1>Standort ${blick1.standort}</h1>
-        <p>Höhe: ${blick1.seehoehe} m </p>
-        <em>Kunde: ${blick1.kunde}</em>`);
-    
-        }
-    
+        `<h1>Standort ${blick.standort}</h1>
+        <p>Höhe: ${blick.seehoehe} m </p>
+        <em>Kunde: ${blick.kunde}</em>`);
+
+}
+
+console.log(blickeGruppe.getBounds());
+
+//Auf Adlerblicke zoomen
+karte.fitBounds(blickeGruppe.getBounds());
---
+++ adlerweg/blicke.js
@@ -0,0 +1,107 @@
+const ADLERBLICKE = [{
+        "kunde": "Wilder Kaiser",
+        "standort": "Gruttenhütte",
+        "seehoehe": 1640,
+        "lat": "47.55564",
+        "lng": "12.31861"
+    },
+    {
+        "kunde": "Bergbahn Scheffau",
+        "standort": "Brandstadl",
+        "seehoehe": 1640,
+        "lat": "47.49120",
+        "lng": "12.24800"
+    },
+    {
+        "kunde": "Lechtal Tourismus",
+        "standort": "Sonnalm Jöchelspitze",
+        "seehoehe": 1786,
+        "lat": "47.27528",
+        "lng": "10.36505"
+    },
+    {
+        "kunde": "TVB Region Hall-Wattens",
+        "standort": "Kugelwald am Glungezer",
+        "seehoehe": 1541,
+        "lat": "47.40785",
+        "lng": "11.91222"
+    },
+    {
+        "kunde": "TVB St. Anton am Arlberg",
+        "standort": "Kapall",
+        "seehoehe": 2330,
+        "lat": "47.14805",
+        "lng": "10.24917"
+    },
+    {
+        "kunde": "Tourismusverband Achensee",
+        "standort": "Am Rofan, neben dem Almstüberl, gegenüber der Rofanseilbahn Bergstation",
+        "seehoehe": 1831,
+        "lat": "47.44154",
+        "lng": "11.76364"
+    },
+    {
+        "kunde": "Tourismusverband Achensee",
+        "standort": "Am Zwölferkopf, direkt beim Alpengasthaus Karwendel am Balkon",
+        "seehoehe": 1491,
+        "lat": "47.42783",
+        "lng": "11.69493"
+    },
+    {
+        "kunde": "Ferienregion Hohe Salve  ",
+        "standort": "Buchacker Alm Adlerhorst",
+        "seehoehe": 1460,
+        "lat": "47.53789",
+        "lng": "12.02317"
+    },
+    {
+        "kunde": "Alpbachtal",
+        "standort": "Berghaus Sonnwendjoch",
+        "seehoehe": 1780,
+        "lat": "47.46408",
+        "lng": "11.82863"
+    },
+    {
+        "kunde": "TVB Imst",
+        "standort": "Muttekopf",
+        "seehoehe": 2050,
+        "lat": "47.25566",
+        "lng": "10.67688"
+    },
+    {
+        "kunde": "TVB Kufstein",
+        "standort": "Brentenjoch",
+        "seehoehe": 1226,
+        "lat": "47.57842",
+        "lng": "12.21217"
+    },
+    {
+        "kunde": "Nordkette Innsbruck",
+        "standort": "Seegrube",
+        "seehoehe": 1905,
+        "lat": "47.30623",
+        "lng": "11.37941"
+    },
+    {
+        "kunde": "Nordkette Innsbruck",
+        "standort": "Hafelekar",
+        "seehoehe": 2269,
+        "lat": "47.31185",
+        "lng": "11.38376"
+    },
+    {
+        "kunde": "TVB Wilder Kaiser",
+        "standort": "Going Astberg Speicherteich 2",
+        "seehoehe": 1250,
+        "lat": "47.50083",
+        "lng": "12.32417"
+    },
+    {
+        "kunde": "TVB Wilder Kaiser",
+        "standort": "Going Astberg Speicherteich 2",
+        "seehoehe": 1250,
+        "lat": "47.50167",
+        "lng": "12.32306"
+    }
+
+];
