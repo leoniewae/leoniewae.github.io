0573f98d1b2e2bd47d41ff2eb97a68afc554c14b
Wächter <csav8898@uibk.ac.at>
2019-03-27 18:06:15 +0100

    Adlerweg

--- adlerweg/adlerweg.js
+++ adlerweg/adlerweg.js
@@ -15,7 +15,7 @@ let karte = L.map ("map");
 
 //auf Ausschnitt zoomen
 karte.setView (
-    [breite,laenge],
+    [breite1,laenge1],
     13 // Zoomfaktor 1= ganze Welt bis 25= sehr nah
 
 );
@@ -24,13 +24,64 @@ karte.setView (
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 //positionsmarker hinzufügen
-let pin = L.marker(
+let pin1 = L.marker(
    [breite1,laenge1]
 ).addTo(karte);
-let pin = L.marker(
+
+let pin2 = L.marker(
     [breite2,laenge2]
  ).addTo(karte);
 
 //popup zum Pin hängen
-pin.bindPopup(titel1).openPopup();
-pin.bindPopup(titel2).openPopup();
+pin1.bindPopup(titel1).openPopup();
+pin2.bindPopup(titel2).openPopup();
+
+const blick1 = {
+    kunde: "Wilder Kaiser",
+    standort: "Gruttenhütte",
+    seehoehe: "1640",
+    lat: 47.55564,
+    lng: 12.31861
+};
+
+let pin3 = L.marker (
+    [blick1.lat, blick1.lng]
+).addTo(karte);
+
+
+    const blick2 = {
+        kunde: "Bergbahn Scheffau",
+        standort: "Brandstadl",
+        seehoehe: "1640",
+        lat: 44.4912,
+        lng: 12.24522 };
+
+    let pin4 = L.marker (
+        [blick2.lat, blick2.lng]
+    ).addTo(karte);
+    
+
+        const blick3 = {
+            kunde: "Lechtal Tourismus",
+            standort: "Sonnalm Jöchelspitz",
+            seehoehe: "1786",
+            lat: 47.41028,
+            lng: 10.60083};
+
+
+        const adlerblicke =[
+            blick1,
+            blick2,
+            blick3
+        ];
+
+        for (let blick of adlerblicke) {
+let blickpin = L.marker ( [blick.lat, blick.lng]
+    ).addTo(karte);
+    blickpin.bindPopup(
+        `<h1>Standort ${blick1.standort}</h1>
+        <p>Höhe: ${blick1.seehoehe} m </p>
+        <em>Kunde: ${blick1.kunde}</em>`);
+    
+        }
+    
