168acd9700eb960f40fed9b2974d4c6df5bd800a
Wächter <csav8898@uibk.ac.at>
2019-04-03 15:56:04 +0200

    Quelle der Karte

--- adler/main.js
+++ adler/main.js
@@ -21,7 +21,11 @@ karte.setView(
 );
 
 //Openstreet Map hinzufügen
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png",{
+ subdomains: ["a","b", "c"],
+ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+
+}).addTo(karte);
 
 //positionsmarker hinzufügen
 let pin1 = L.marker(
