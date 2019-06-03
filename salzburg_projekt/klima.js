d914ba334b80aa844c0795551d836e60c1abb638
leoniewae <leonie_waechter@live.de>
2019-06-03 13:40:05 +0200

    Salzburg Projekt

---
+++ salzburg_projekt/klima.js
@@ -0,0 +1,14 @@
+let karte = L.map("map");
+
+const kartenlayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }),
+    geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+};
+
+kartenlayer.geolandbasemap.addTo(karte);
---
+++ salzburg_projekt/stadt.js
@@ -0,0 +1,45 @@
+let karte = L.map("map");
+
+const kartenlayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }),
+    geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),  
+    bmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+    bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+    stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",{
+       subdomains: ["a", "b","c"],
+       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
+    }),
+
+
+};
+
+kartenlayer.geolandbasemap.addTo(karte);
+
+L.control.layers ({
+    "Geoland Basemap": kartenlayer.geolandbasemap,
+    "Geoland Basemap Orthofoto": kartenlayer.bmaporthofoto30cm,
+    "Geoland Basemap Gelände": kartenlayer.bmapgelaende,
+    "Geoland Basemap Oberfläche": kartenlayer.bmapoberflaeche, 
+    "Stamen Watercolor": kartenlayer.stamen_watercolor
+    }).addTo(karte);
+
+
+
+karte.setView(
+    [47.80814,11.04209], 13);
