85082b88d2ba324944e04ef56fd0005c290b2cd4
Wächter <csav8898@uibk.ac.at>
2019-06-04 11:58:30 +0200

    Projekt weiterarbeit

--- salzburg_projekt/klima.js
+++ salzburg_projekt/klima.js
@@ -9,15 +9,50 @@ const kartenlayer = {
         subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
         attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
     }),
+    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+    bmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+    bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+    stamen_relief: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg",{
+        subdomains: ["a", "b","c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+    }),
+    stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",{
+       subdomains: ["a", "b","c"],
+       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
+    }),
+
+
 };
 
 kartenlayer.geolandbasemap.addTo(karte);
 
+
 L.control.layers ({
-    "Geoland Basemap": kartenlayer.geolandbasemap
+    "Geoland Basemap": kartenlayer.geolandbasemap,
+    "Geoland Basemap Orthofoto": kartenlayer.bmaporthofoto30cm,
+    "Geoland Basemap Gelände": kartenlayer.bmapgelaende,
+    "Stamen Relief": kartenlayer.stamen_relief,
+    "Stamen Watercolor": kartenlayer.stamen_watercolor
     }).addTo(karte);
 
 
 
 karte.setView(
-    [47.267222,11.392778], 13);
+    [47.80814, 11.04209],8);
+
+karte.addControl(new L.Control.Fullscreen());
+
+const scale = L.control.scale({
+    imperial: false,
+    metric: true,
+});
+karte.addControl(scale);
--- salzburg_projekt/stadt.js
+++ salzburg_projekt/stadt.js
@@ -12,7 +12,7 @@ const kartenlayer = {
     bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
         subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
         attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
-    }),  
+    }),
     bmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
         subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
         attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
@@ -20,6 +20,10 @@ const kartenlayer = {
     bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpeg", {
         subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
         attribution: 'Datenquelle: <a href="https//www.basemap.at">basemap.at</a>'
+    }),
+    stamen_relief: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg",{
+        subdomains: ["a", "b","c"],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
     }),
     stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",{
        subdomains: ["a", "b","c"],
@@ -31,18 +35,17 @@ const kartenlayer = {
 
 kartenlayer.geolandbasemap.addTo(karte);
 
+
 L.control.layers ({
     "Geoland Basemap": kartenlayer.geolandbasemap,
     "Geoland Basemap Orthofoto": kartenlayer.bmaporthofoto30cm,
     "Geoland Basemap Gelände": kartenlayer.bmapgelaende,
-    "Geoland Basemap Oberfläche": kartenlayer.bmapoberflaeche, 
+    "Stamen Relief": kartenlayer.stamen_relief,
     "Stamen Watercolor": kartenlayer.stamen_watercolor
     }).addTo(karte);
 
-    kartenLayer.geolandbasemap.addTo(karte);
-
 karte.setView(
-    [47.80814,11.04209], 13);
+    [47.80814, 11.04209], 8);
 
 karte.addControl(new L.Control.Fullscreen());
 
@@ -51,3 +54,11 @@ const scale = L.control.scale({
     metric: true,
 });
 karte.addControl(scale);
+
+let salzburg =L.marker (
+    [47.808955, 13.037266]
+    ).addTo(karte)
+
+let gaisberg =L.marker(
+    [47.803598, 13.111224]
+).addTo(karte)
